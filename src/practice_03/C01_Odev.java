2be754f5e022eed50eaa1ca69be7cbfe1dc29723
cayten <caytenn@gmail.com>
2023-06-07 20:12:31 +0300

    java

--- src/practice_03/C01_Odev.java
+++ src/practice_03/C01_Odev.java
@@ -1,2 +1,10 @@
-package practice_03;public class C01_Odev {
+package practice_03;
+
+public class C01_Odev {
+    /*
+given an int array and find the squares of the elements
+(Verilen bir int dizisi icin elemanlarin karelerini bulun )
+Example:{2,6,4,5,8,9}
+output:{4,36,16,25,64,81}
+*/
 }
